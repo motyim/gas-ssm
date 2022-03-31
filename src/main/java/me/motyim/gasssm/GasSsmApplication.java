35b7191f0c1e123f260c68db10f58ac182b4f97d
Motyim <mohamed.motyim@blockchaingroup.io>
2022-03-31 23:00:50 +0200

    feat: init repo

---
+++ src/main/java/me/motyim/gasssm/GasSsmApplication.java
@@ -0,0 +1,13 @@
+package me.motyim.gasssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasSsmApplication.class, args);
+    }
+
+}
